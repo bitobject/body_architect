fd31cd531b5f7299f6cf2a1241b7bf1d2e0b036f
One Object <haxttanak@gmail.com>
2024-03-05 14:35:47 +0700

    seventh

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BodyArchitect.MixProject do
   def project do
     [
       app: :body_architect,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
